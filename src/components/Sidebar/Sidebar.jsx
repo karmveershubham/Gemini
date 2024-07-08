3ea4a3160b9d62b448536da60741ad67aa5beb09
karmveershubham <s.karmveer1201@gmail.com>
2024-07-08 22:16:28 +0530

    Creating Gemini -Clone

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import './Sidebar.css';
+import {assets} from  '../../assets/assets'
+const Sidebar = ()=>{
+    return (
+    <div className="sidebar">
+        <div className="top">
+            <img className="menu"  src={assets.menu_icon} alt=""/>
+            <div className="new-chat">
+                <img src={assets.plus_icon} alt =""/>
+                <p>New Chat</p>
+            </div>
+            <div className="recent">
+                <p className="recent-title">Recent</p>
+                <div className="recent-entry"></div>
+                <img src={assets.message_icon} alt=""/>
+                <p> What is React ...</p>
+
+            </div>
+        </div>
+        <div className="bottom">
+            <div className="bottom-item recent-history " >
+            <img src={assets.question_icon} alt=""/>
+            <p> Help </p>
+            </div>
+            <div className="bottom-item recent-history">
+                <img src={assets.history_icon} alt=""/>
+                <p>Activity</p>
+            </div>
+            <div className="bottom-item recent-history">
+                <img src={assets.settings_icon} alt=""/>
+                <p>Settings</p>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Sidebar;
